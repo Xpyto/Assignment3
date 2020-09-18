5b5cc14e459d071017553864ec1a3a0a1098ee98
Xpyto <0rickcir0@gmail.com>
2020-09-18 20:32:44 +1000

    audio input
    attached audio to game
    added background music

---
+++ Assets/Scripts/onCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCollision : MonoBehaviour
+{
+    AudioSource eat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        eat = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void onCollisionEnter(Collision collision)
+    {
+        eat.Play();
+        gameObject.SetActive(false);
+    }
+}
